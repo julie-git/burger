b1b10fe15972acbc690f9215881d538eadd9e287
JG <noreply@jg.com>
2020-01-18 00:03:28 -0800

    changed connection.js for deploy to Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,16 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "teddy",
   database: "burgers_db"
-  
-});
+ });
+};
 
 // Make connection.
 connection.connect(function(err) {
